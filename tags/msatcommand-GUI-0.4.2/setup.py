a3069a6f5491fb35f8c68b17bb4a73180fbc4f38
faircloth <faircloth@cddf4735-3325-0410-b006-0d3caba88fa1>
2007-02-23 00:11:26 +0000

    adding 0.4.2 to tags

---
+++ tags/msatcommand-GUI-0.4.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin
+"""
+setup.py - script for building MyApplication
+
+Usage:
+    % python setup.py py2app
+"""
+from distutils.core import setup
+import os, sys
+
+if os.name == 'posix':
+    import py2app
+    setup(
+        name='msatCommand',
+        version='0.4.2',
+        description='python searching of fasta files for microsat repeats',
+        author='Brant C. Faircloth',
+        author_email='brant@uga.edu',
+        license='GPL',
+        app=['msatCommandGui.py'],
+        options=dict(py2app=dict(
+                includes=['Bio.Fasta'],
+                packages=['Bio'],
+                plist=dict(
+                    LSPrefersPPC=True,
+                ),
+            )),
+    )
+    
+elif os.name == 'nt':
+    import py2exe
+    setup(
+        name='msatCommand',
+        version='0.4.2',
+        description='python searching of fasta files for microsat repeats',
+        author='Brant C. Faircloth',
+        author_email='brant@uga.edu',
+        license='GPL',
+        windows=[
+            {
+                'script':'msatCommandGuiMod.py',
+                'icon_resources':[(1, 'gmconvert_icon_files2.ico')]
+            }
+        ],
+    )
+    
+else:
+    sys.exit
